2207c5178214afcddaf8e7d9483b1c7098428707
Itz-thevine <divine2christ@yahoo.com>
2021-12-30 20:42:46 +0100

    added day Nineteen

---
+++ dayNineteen/dayNineteen.js
@@ -0,0 +1,20 @@
+// Define a function which can generate a dictionary where the keys are numbers between 1 and
+// 20 (both included) and 
+// the values are square of keys. The function should just print the keys only.
+
+dictionary();
+var dict;
+
+function dictionary () {
+    for (let i = 1; i <= 20; i++) {
+        var dict = {
+            i : i**2
+        }
+        console.log(Math.sqrt(dict.i))
+    }
+   
+}
+
+
+
+// console.log('hello');
